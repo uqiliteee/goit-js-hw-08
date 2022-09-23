970f14cdbdd35c92b2383e9faddb03f58e5872ed
Karina <meriucakarina@gmail.com>
2022-09-23 23:35:43 +0300

    third task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,29 +1,49 @@
-// import throttle from 'lodash.throttle';
-// import { save, load, remove } from './storage';
+import throttle from 'lodash.throttle';
 const formRef = document.querySelector('.feedback-form');
+const inputRef = document.querySelector('.feedback-form input');
+const textareaRef = document.querySelector('.feedback-form textarea');
 const LOCAL_STORAGE_KEY = 'feedback-form-state';
-initPage();
 
-formRef.addEventListener('input', onFormInput);
-formRef.addEventListener('submit', onFormSubmit);
 const formData = {};
 
+savingData();
+
 const onFormInput = (event) => {
-    const {name, value} = event.target
-    formData[name]= value
-    // console.log(formData);
-    localStorage.setItem(LOCAL_STORAGE_KEY, formData)
-};
+    formData[event.target.name] = event.target.value;
 
-
-
-function initPage() {
-    const savingData = localStorage.getItem(LOCAL_STORAGE_KEY);
-    console.log(savingData);
-    if (!saveData) {
-        const parseData = JSON.parse(formData);
-        console.log(parseData)
-    }
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData))
     
+};
+formRef.addEventListener('input', onFormInput);
+
+
+const onFormSubmit = event => {
+    event.preventDefault();
+    event.currentTarget.reset();
+    const getData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    console.log(getData)
+
+    localStorage.removeItem(LOCAL_STORAGE_KEY);
+};
+formRef.addEventListener('submit', throttle(onFormSubmit, 500));
+
+function savingData() {
+    const getData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+
+    if (getData === null) {
+        return
+    };
+
+    if (getData.email) {
+        inputRef.value = savingData.email;
+        formData['email'] = inputRef.value;
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
+    };
+    if (getData.message) {
+        textareaRef.value = savingData.message;
+        formData['message'] = textareaRef.value;
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
+    };
 
 }
+
